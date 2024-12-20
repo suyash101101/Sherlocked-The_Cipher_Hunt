7cd861b7d421a10e6e9b4ad1e44e48f9d40b2095
AnshumanDave <87855120+AnshumanDave@users.noreply.github.com>
2024-12-21 00:20:07 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import {supabase} from '../client';
 
 const SignUp = () => {
@@ -69,6 +70,7 @@ const SignUp = () => {
         </button>
 
       </form>
+      Already have an account ? <Link to= '/'>login</Link>
     </div>
   )
 }
