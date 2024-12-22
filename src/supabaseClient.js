dd621a379da52c1829995b373476c3002e393fed
AnshumanDave <87855120+AnshumanDave@users.noreply.github.com>
2024-12-22 22:46:01 +0530

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://sroeexbdokczmzngdmte.supabase.co'; // Replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyb2VleGJkb2tjem16bmdkbXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3MjMyNTEsImV4cCI6MjA1MDI5OTI1MX0.IEsAZs_1Mcz9lanJ7w9PC-PTwktOOWwGXtk7PlBZpPY'; // Replace with your Supabase anon key
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
