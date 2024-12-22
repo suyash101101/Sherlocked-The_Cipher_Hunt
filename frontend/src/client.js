e84fdba65632f4b28352fee557c4b785af054223
AnshumanDave <87855120+AnshumanDave@users.noreply.github.com>
2024-12-22 19:39:07 +0530

    Delete frontend directory

--- frontend/src/client.js
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://efcastawdxpnkeqlaeea.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmY2FzdGF3ZHhwbmtlcWxhZWVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1NTUwMjQsImV4cCI6MjA1MDEzMTAyNH0.KdM3D0tqqXj-iJfwFJw17nH0QKpSXN6wjlbcYHJPFt8'
-export const supabase = createClient(supabaseUrl, supabaseKey)
--- frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import login from './login'
-import SignUp from './SignUp'
-import Homepage from './Homepage'
-
-export {
-    login, SignUp, Homepage
-}
